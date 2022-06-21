eeb83818617326a0f2fdbd3427c3df1d8ef07b55
Pawel S <83219267+szpej@users.noreply.github.com>
2022-06-21 22:31:00 +0200

    Update app.R

--- app.R
+++ app.R
@@ -1,48 +1,42 @@
-
-
 library(shiny)
-library(tidyverse)
+library(dplyr)
+library(tidyr)
+library(stringr)
 library(sf)
+library(ggplot2)
 
-dane <- read_csv("dane.csv")
-dane_woj <- dane[is.na(dane$Liczba_miast),1]
-dane_type <-  dane[!(is.na(dane$Liczba_miast)),1]
-dane_dane <- dane[!(is.na(dane$Liczba_miast)),2]
-dane_dane2 <- dane[!(is.na(dane$Liczba_miast)),3]
-dane <- 
-  tibble(woj = rep(dane_woj$Zmienna_1, each = 4),
-         wielkosc = dane_type$Zmienna_1, 
-         liczba = dane_dane$Liczba_miast,
-         ludnosc = dane_dane2$Ludnosc)
+
+load("woj_id.RData")
+woj_id <- woj_id %>% dplyr::arrange(name)
+woj_id$name <- str_to_lower(woj_id$name )
 woj <- st_read("mapa/gadm40_POL_1.shp")
-total_dane <- left_join(woj, dane, by = c("NAME_1" = "woj"))
-rm(dane_dane, dane_type, dane_woj, dane_dane2, dane, woj)
+woj <- woj %>% dplyr::arrange(NAME_1)
+woj$NAME_1 <- str_to_lower(woj$NAME_1 )
 
 
-generate_map <- function(data, variable, type) {
-  date[date$wielkosc == type,]
-  ggplot(data=data) +
-    geom_sf(aes(fill = variable)) +
-    xlab("Długość geograficzna")+ylab("Szerokość geograficzna") +
-    xlab("") + 
-    ylab("") 
-
+get_data <- function(x) {
+  my_file <- paste(paste("test_", x, sep = ""), ".csv", sep = "")
+  read.csv(my_file)
 }
+id_data <- tibble(id = c("a", "b"),
+                       variable = c("Ilość", "Małżeństwa"))
+
+
 
 ui <- fluidPage(
   titlePanel("Miasta w liczbach"
   ),
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId = "variable",
-                  label = "Variable",
-                  choices =  names(total_dane)[13:14]),
+      selectInput(inputId = "main_variable",
+                  label = "Cecha",
+                  choices =  c("Ilość", "Małżeństwa") ),
       selectInput(inputId = "size",
-                  label = "Size",
-                  choices = unique(total_dane$wielkosc)),
+                  label = "Wielkość",
+                  choices = c("Ogółem", "poniżej 20000", "20000 - 99999", "100000 i więcej")),
       numericInput(inputId = "year", 
                    label = "Rok", 
-                   value = 2020, min = 2010, max = 2020),
+                   value = 2020, min = 1999, max = 2020),
       downloadButton("download", "Download .csv")
       ),
   mainPanel(
@@ -56,39 +50,45 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  title <- reactive(input$variable)
-  subtitle <- reactive(input$size)
-  year_ <- reactive(input$year)
-  data <- reactive(
-    st_drop_geometry(total_dane) %>% filter(wielkosc == input$size) %>%
-    select("NAME_1", .data[[input$variable]])
-  )
+  data_ready <- reactive({
+    not_ready <- get_data(id_data$id[id_data$variable == input$main_variable])
+    not_ready %>% filter(year == input$year) %>% select(3:18) %>%
+      pivot_longer(names_to = "id", values_to = "variable", cols = everything()) %>%
+      mutate(id = str_remove(string = .$id, pattern = "val_")) %>%
+      left_join(woj_id, by = "id")
+  })
+  
+  data_to_map <- reactive(left_join(woj, data_ready(), by = c("NAME_1" = "name")))
+  year_var <- reactive(input$year)
+  main_var <- reactive(input$main_variable)
+  
   
   output$my_map <- renderPlot({
-    total_dane %>% filter(wielkosc == input$size) %>%
+    data_to_map() %>%
       ggplot() +
-      geom_sf(aes(fill = .data[[input$variable]] )) +
+      geom_sf(aes(fill = variable )) +
       xlab("Długość geograficzna")+ylab("Szerokość geograficzna") +
       xlab("") + 
-      ylab("")  +
-      ggtitle(title(), 
-              subtitle = paste("W roku ",year_())) +
-      labs(fill=subtitle())  +
+      ylab("") +
       scale_fill_gradient(low = "#ffffcc", high = "#006837") +
       geom_sf_text(mapping=aes(label=NAME_1),size=3) +
-      geom_sf_text(mapping=aes(label=.data[[input$variable]]),size=3,nudge_y = -0.2)  +
+      geom_sf_text(mapping=aes(label= variable),size=3,nudge_y = -0.2)  +
+      ggtitle(main_var(),
+              subtitle = paste("W roku ", year_var(), sep = "")) +
+      labs(fill=main_var()) + 
       theme(axis.ticks = element_blank(),
             axis.text = element_blank(),
             rect = element_blank(), # usuwa siatkę
             legend.position = c(0.1,0.15),legend.title.align=0.5, legend.text.align = 1 )
-}, res = 96)
-
+  }, res = 120)
+  
+  
   output$download <- downloadHandler(
     filename = function() {
       paste0("test", ".csv")
     },
     content = function(file) {
-      write_csv(data(), file)
+      write.csv(data_ready(), file)
     }
   )
   
